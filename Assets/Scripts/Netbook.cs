12fe508b5adc004c4bdcd16acfd4cac2770b760c
AgusBaudot <101721143+AgusBaudot@users.noreply.github.com>
2022-08-05 10:56:37 -0300

    Spawn nets when near cart

---
+++ Assets/Scripts/Netbook.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Netbook : MonoBehaviour
+{
+    public GameObject net;
+    public Transform SpawnPoint;
+    public Text AvisoSpawn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerStay(Collider col)
+    {
+        if(col.gameObject.tag == "Player" && Input.GetKey(KeyCode.N))
+        {
+            Instantiate(net, SpawnPoint.position, gameObject.transform.rotation);
+        }
+        if(col.gameObject.tag == "Player")
+        {
+            AvisoSpawn.text = "Estas cerca de un carrito, podés spawnear una netbook presionando la tecla N";
+        }
+    }
+}
